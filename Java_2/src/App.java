e80276dcf24ce9a89de18e7478bd2343ffd6cfe5
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2023-11-12 18:01:46 -0300

    aaa

--- Java_2/src/App.java
+++ Java_2/src/App.java
@@ -1,13 +1,17 @@
 import java.util.Locale;
+import java.util.Scanner;
 
 import entities.Departments;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Locale.setDefault(Locale.US)
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter department's name: ");
-        String name = sc.next
+        String name = sc.nextLine();
+
+        sc.close();
 
     }
 }
