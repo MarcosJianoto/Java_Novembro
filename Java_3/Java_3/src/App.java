9a26dec99cfb4c7fdcc1f624e784004376c7bfb9
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2023-11-12 18:40:56 -0300

    commit1

---
+++ Java_3/Java_3/src/App.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um valor: ");
+        int n = sc.nextInt();
+        double[] vect = new double[n];
+
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            vect[i] = sc.nextDouble();
+            sum += vect[i];
+        }
+        double avg = sum / n;
+
+        System.out.println("AVERAGE HEIGHT: " + avg);
+
+        sc.close();
+
+    }
+
+}
