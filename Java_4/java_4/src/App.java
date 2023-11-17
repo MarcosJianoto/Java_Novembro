31087f09ad1f52a05c9c6c0e8d7bf3649494e2b4
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2023-11-16 21:36:42 -0300

    commit

--- Java_4/java_4/src/App.java
+++ Java_4/java_4/src/App.java
@@ -5,29 +5,27 @@ import entities.Product;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite um numero: ");
         int n = sc.nextInt();
 
         Product[] vect = new Product[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < vect.length; i++) {
             sc.nextLine();
-            System.out.println("Name Product: ");
             String name = sc.nextLine();
-            System.out.println("Price Product: ");
             double price = sc.nextDouble();
             vect[i] = new Product(name, price);
         }
 
         double sum = 0.0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < vect.length; i++) {
             sum += vect[i].getPrice();
         }
-        double avg = sum / n;
 
-        System.out.printf("%.2f%n", avg);
+        double avg = sum / vect.length;
+        System.out.println(avg);
 
         sc.close();
 
