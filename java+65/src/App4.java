5f51f33b0f152c623876e63d8e1e37e8e1449424
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2024-01-23 19:47:30 -0300

    apps5

--- java+65/src/App4.java
+++ java+65/src/App4.java
@@ -1,4 +1,5 @@
 import java.util.Locale;
+import java.util.Random;
 import java.util.Scanner;
 
 public class App4 {
@@ -6,9 +7,28 @@ public class App4 {
   public static void main(String[] args) throws Exception {
     Locale.setDefault(Locale.US);
     Scanner sc = new Scanner(System.in);
+    Random random = new Random();
 
-    
+    while (true) {
+      System.out.println("Digite um numero entre 1 e 2");
+      int j = sc.nextInt();
+      sc.nextLine();
 
-    sc.close();
+      int numeroAleatorio = random.nextInt(2) + 1;
+      System.out.println(numeroAleatorio);
+
+      if (j == numeroAleatorio) {
+        System.out.println("Conseguiu!");
+        System.out.println("Gostaria de tentar novamente?");
+        String tentarNovamente = sc.nextLine();
+        if (tentarNovamente.equals("n")) {
+          break;
+        } else {
+          continue;
+        }
+      } else {
+        System.out.println("Não conseguiu");
+      }
+    }
   }
 }
---
+++ java+65/src/App5.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
+public class App5 {
+
+  public static void main(String[] args) throws Exception {
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    
+
+
+    sc.close();
+  }
+}
