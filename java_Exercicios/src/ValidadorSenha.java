2817b8b8c54452f5620e6ad7398bc6a9d2ad2096
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2024-01-30 20:31:43 -0300

    commitando

--- java_Exercicios/src/ValidadorSenha.java
+++ java_Exercicios/src/ValidadorSenha.java
@@ -1,7 +1,7 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-public class App18 {
+public class ValidadorSenha {
 
   public static void main(String[] args) throws Exception {
     Locale.setDefault(Locale.US);
