20e7447fb4f9d4f0ad370f68388b223ef8d25154
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2024-01-30 18:15:25 -0300

    pares_impares

--- java_66/src/App13.java
+++ java_66/src/App13.java
@@ -7,6 +7,35 @@ public class App13 {
     Locale.setDefault(Locale.US);
     Scanner sc = new Scanner(System.in);
 
+    //Contagem de numeros pares e impares
+    int n = 3;
+    int[][] vect = new int[n][n];
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        vect[i][j] = sc.nextInt();
+      }
+    }
+
+    System.out.print("pares: ");
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (vect[i][j] % 2 == 0) {
+          System.out.print(vect[i][j] + " ");
+        }
+      }
+    }
+
+    System.out.println();
+    System.out.print("impares: ");
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (vect[i][j] % 2 != 0) {
+          System.out.print(vect[i][j] + " ");
+        }
+      }
+    }
+
     sc.close();
   }
 }
