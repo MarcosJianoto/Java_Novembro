7dae00d8cd748462b16e66c80ea95c42471fb716
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2024-01-31 17:17:40 -0300

    31/01

--- java_Exercicios/src/CalculoJurosCompostos.java
+++ java_Exercicios/src/CalculoJurosCompostos.java
@@ -7,8 +7,12 @@ public class CalculoJurosCompostos {
     Locale.setDefault(Locale.US);
     Scanner sc = new Scanner(System.in);
 
-    
-
+    //anos para calcular
+    double P = 25000.00; //investimento inicial
+    double n = 10.0; // anos deixando o dinheiro investido
+    double r = 10.0 / 100.0; // % de juros anual.
+    double A = P * Math.pow(1.0 + r, n);
+    System.out.println(A);
 
     sc.close();
   }
--- java_Exercicios/src/ContagemVogaisEConsoantes.java
+++ java_Exercicios/src/ContagemVogaisEConsoantes.java
@@ -7,8 +7,42 @@ public class ContagemVogaisEConsoantes {
     Locale.setDefault(Locale.US);
     Scanner sc = new Scanner(System.in);
 
-    
+    String palavra = sc.nextLine();
+    String palavraToUpper = palavra.toLowerCase();
+    char temp;
 
+    System.out.println("Vogal: ");
+    for (int i = 0; i < palavraToUpper.length(); i++) {
+      temp = palavraToUpper.charAt(i);
+      if (
+        temp == 'a' ||
+        temp == 'e' ||
+        temp == 'i' ||
+        temp == 'o' ||
+        temp == 'u' ||
+        temp == 'y'
+      ) {
+        System.out.print(temp + " ");
+      }
+    }
+
+    System.out.println();
+
+    System.out.println("Consoantes: ");
+    for (int i = 0; i < palavraToUpper.length(); i++) {
+      temp = palavraToUpper.charAt(i);
+
+      if (
+        temp != 'a' &&
+        temp != 'e' &&
+        temp != 'i' &&
+        temp != 'o' &&
+        temp != 'u' &&
+        temp != 'y'
+      ) {
+        System.out.println(temp);
+      }
+    }
 
     sc.close();
   }
