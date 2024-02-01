47299d7a0011d8d4f1281f5feecbf24d275bb0fb
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2024-02-01 20:43:05 -0300

    validadorCPF

--- java_Exercicios/src/ValidadorCPF.java
+++ java_Exercicios/src/ValidadorCPF.java
@@ -7,9 +7,110 @@ public class ValidadorCPF {
     Locale.setDefault(Locale.US);
     Scanner sc = new Scanner(System.in);
 
-    
+    //array com os digitos ou uma string?
+    String CPFString = "10622379941"; //no caso, os 9 primeiros digitos utilizamos pesos
+    int[] CPFarray = new int[CPFString.length()];
+    int[] CPFarrayPosValid = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    int[] CPFarrayPosValid2 = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
+    for (int i = 0; i < CPFString.length(); i++) {
+      CPFarray[i] = Character.getNumericValue(CPFString.charAt(i));
+      System.out.println(CPFarray[i]);
+    }
 
-    sc.close();
+    System.out.println("----------------------------------");
+
+    while (true) {
+      if (CPFarray.length != 11) {
+        System.out.println("Errado, digite novamente");
+        CPFString = sc.nextLine();
+      }
+
+      int peso9 = 10;
+
+      if (CPFarray.length == 11) {
+        for (int i = 0; i <= CPFarray.length - 3; i++) {
+          CPFarrayPosValid[i] = CPFarray[i] * peso9;
+          System.out.print(" " + CPFarrayPosValid[i] + " ");
+          peso9 -= 1;
+        }
+
+        int soma = 0;
+        for (int i = 0; i < CPFarrayPosValid.length; i++) {
+          soma += CPFarrayPosValid[i];
+          System.out.println();
+          System.out.println(soma);
+        }
+
+        System.out.println("Divisão por onze: ");
+        int divisaoOnze = soma % 11;
+        System.out.println(divisaoOnze);
+        //resto = 3;
+
+        System.out.println("Subtrair o resto por 11");
+        int subtrairResto = 11 - divisaoOnze;
+        //11 - 3 = 8;
+
+        if (subtrairResto >= 10) {
+          System.out.println("primeiro digito verificador = 0");
+        } else {
+          System.out.println("seu 1º digito verificador é: " + subtrairResto);
+        }
+
+        int[] CPFarrayDigitoVerificador = new int[] {
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+        };
+
+        for (int i = 0; i < CPFarrayDigitoVerificador.length - 1; i++) {
+          CPFarrayDigitoVerificador[i] = CPFarray[i];
+          System.out.println(CPFarrayDigitoVerificador[i]);
+        }
+        for (int i = 9; i < CPFarrayDigitoVerificador.length; i++) {
+          CPFarrayDigitoVerificador[i] = subtrairResto;
+          System.out.println(CPFarrayDigitoVerificador[i]);
+        }
+
+        int soma2 = 0;
+        int peso10 = 11;
+        for (int i = 0; i <= CPFarrayDigitoVerificador.length - 1; i++) {
+          CPFarrayPosValid2[i] = CPFarrayDigitoVerificador[i] * peso10;
+          System.out.print(" " + CPFarrayPosValid2[i] + " ");
+          peso10 -= 1;
+          soma2 += CPFarrayPosValid2[i];
+        }
+        System.out.println(soma2);
+
+        System.out.println("Divisão por onze: ");
+        int divisaoOnze2 = soma2 % 11;
+        System.out.println(divisaoOnze2);
+        //resto = 2;
+
+        System.out.println("Subtrair o resto por 11");
+        int subtrairResto2 = 11 - divisaoOnze2;
+        System.out.println(subtrairResto2);
+        //11 - 3 = 8;
+
+        System.out.println("seu 1º digito verificador é: " + subtrairResto);
+        System.out.println("seu 2º digito verificador é: " + subtrairResto2);
+
+        break;
+      }
+
+      sc.close();
+    }
+  }
+
+  private static char[] CPFarray(int i) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'CPFarray'");
   }
 }
