2165532a6acd9b14c8c68c9c092448c0d3a26e0c
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2024-02-04 12:44:17 -0300

    aprendendo

--- Curso_ITA_Desenvolvimento_Agil/src/App.java
+++ Curso_ITA_Desenvolvimento_Agil/src/App.java
@@ -1,7 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        
+        //hoje usamos o http para transferir figuras, chamadas... tem uma gama muito maior...
+        //uma característica interessante é que é pergunta e resposta. 
+        //o protocolo http nao mantém conexão ativa, toda vez que você pede, ele faz uma nova conexão e depois encerra. 
+        //código 404 = algo nao encontrado.. codigo 500, internal server error.
+        //Scripts CGI === era executado a cada execução, era uma resposta... Quando o Servidor WEb vai rodar, ocmo se fosse linha de comando, ele vai pegar o output que foi gerado... 
+        //Sites com milhares de requisições se tornou impossível realizar diversas ao mesmo tempo. 
+        //no JAVA EE é usado o Servless
 
     }
 }
