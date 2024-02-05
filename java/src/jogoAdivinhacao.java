0111e22d41a05bb0dcd71e79aa2e6af10cf1315f
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2024-02-05 20:26:42 -0300

    jogoAdivinhação

--- java/src/jogoAdivinhacao.java
+++ java/src/jogoAdivinhacao.java
@@ -1,13 +1,35 @@
 import java.util.Scanner;
 
-public class senhaSimples {
+public class jogoAdivinhacao {
 
   public static void main(String[] args) throws Exception {
     //solicitar que o usuário digite uma senha;
     //a senha terá letra e numero;
     Scanner sc = new Scanner(System.in);
 
-    
-    sc.close();
+    //gerei um numero aleatório de 1 a 100;
+    double numberRandom = Math.random() * 100;
+    int numberRandomInt = (int) numberRandom + 1;
+    System.out.println(numberRandomInt);
+
+    System.out.println("Tente adivinhar o numero escolhido pelo computador: ");
+    int numberFind = sc.nextInt();
+
+    while (true) {
+      if (numberFind == numberRandomInt) {
+        System.out.println(
+          "Você descobriu o numero escolhido, que era: " + numberRandomInt
+        );
+        break;
+      }
+
+      if (numberFind != numberRandomInt) {
+        if (numberFind > numberRandomInt) {
+          System.out.println("o numero é maior, digite novamente");
+        }
+      }
+
+      sc.close();
+    }
   }
 }
