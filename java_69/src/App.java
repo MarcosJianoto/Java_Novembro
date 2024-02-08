1d6e8eacf32fceb5ab6d4b36703bc660aa9ba651
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2024-02-07 21:26:38 -0300

    estudando_nelio

---
+++ java_69/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        //classe herda todos dados e comportamento de outra;
+        //vantagens da herança é o REUSO e POLIMORFISMO;
+        //class A extends B;
+
+        
+
+    }
+}
