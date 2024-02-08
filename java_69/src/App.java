2b4e303ae50713ccd69e4f721af3734e90871d2b
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2024-02-08 20:54:48 -0300

    estudandoNelio

--- java_69/src/App.java
+++ java_69/src/App.java
@@ -1,11 +1,21 @@
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        
-        //classe herda todos dados e comportamento de outra;
-        //vantagens da herança é o REUSO e POLIMORFISMO;
-        //class A extends B;
 
-        
+  public static void main(String[] args) throws Exception {
+    Account acc1 = new Account(1, "Marcos", 1000.00);
+    acc1.withdraw(200.0);
+    System.out.println(acc1.getBalance());
 
-    }
+    Account acc2 = new SavingsAccount(2, "null", 1000.00, 0.02);
+    acc2.withdraw(200);
+    System.out.println(acc2.getBalance());
+
+    //palavra super, entender pra que serve.
+    Account acc3 = new BusinessAccount(3, "bob", 1000.00, 500.00);
+    acc3.withdraw(200.00);
+    System.out.println(acc3.getBalance());
+  }
 }
---
+++ java_70/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception 
+    {
+
+        //casting da subclasse para superclasse
+        //uso comum: polimorfismo
+        //palavra instanceof --- equals é importante, porém é bom um pouco mais avançado. 
+
+
+
+
+    }
+}
