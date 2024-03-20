60b56368c0a50613c4a56647d47357efd3f0263a
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2024-03-20 12:53:38 -0300

    commitando

--- java_68/src/entities/CarRental.java
+++ java_68/src/entities/CarRental.java
@@ -7,11 +7,15 @@ public class CarRental {
   private LocalDateTime start;
   private LocalDateTime finish;
 
+  private Vehicle vehicle;
+  private Invoice invoice;
+
   public CarRental() {}
 
-  public CarRental(LocalDateTime start, LocalDateTime finish) {
+  public CarRental(LocalDateTime start, LocalDateTime finish, Vehicle vehicle) {
     this.start = start;
     this.finish = finish;
+    this.vehicle = vehicle;
   }
 
   public LocalDateTime getStart() {
@@ -28,5 +32,21 @@ public class CarRental {
 
   public void setFinish(LocalDateTime finish) {
     this.finish = finish;
+  }
+
+  public Vehicle getVehicle() {
+    return this.vehicle;
+  }
+
+  public void setVehicle(Vehicle vehicle) {
+    this.vehicle = vehicle;
+  }
+
+  public Invoice getInvoice() {
+    return this.invoice;
+  }
+
+  public void setInvoice(Invoice invoice) {
+    this.invoice = invoice;
   }
 }
