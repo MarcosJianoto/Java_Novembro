99b74eaa3ef08330001c91b9d0d7cee977fce1cb
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2024-04-02 06:29:47 -0300

    Automatizado

--- java_100_automatizando/automatizado/src/test/java/automatizado/test/BaseTest.java
+++ java_100_automatizando/automatizado/src/test/java/automatizado/test/BaseTest.java
@@ -1,5 +1,27 @@
 package automatizado.test;
 
-public class BaseTest {
-    
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public abstract class BaseTest {
+
+  public static WebDriver driver;
+  private static final String URL_BASE = "http://www.google.com/";
+  private static final String CAMINHO_DRIVER =
+    "src/test/java/automatizado/resources/chromedriver123.exe ";
+
+  @BeforeClass
+  public static void iniciar() {
+    System.setProperty("webdriver.chrome.driver", CAMINHO_DRIVER);
+    driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get(URL_BASE);
+  }
+
+  @AfterClass
+  public static void finalizar() {
+    driver.quit();
+  }
 }
--- java_100_automatizando/automatizado/src/test/java/automatizado/test/GoogleTest.java
+++ java_100_automatizando/automatizado/src/test/java/automatizado/test/GoogleTest.java
@@ -2,39 +2,33 @@ package automatizado.test;
 
 import static org.junit.Assert.assertTrue;
 
+import automatizado.page.GooglePO;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GoogleTest {
+public class GoogleTest extends BaseTest {
 
-  private WebDriver driver;
-  private final String URL_BASE = "http://www.google.com/";
-  private final String CAMINHO_DRIVER =
-    "src/test/java/automatizado/resources/chromedriver123.exe ";
+  private static GooglePO googlePage;
 
-  private void iniciar() {
-    System.setProperty("webdriver.chrome.driver", CAMINHO_DRIVER);
-    driver = new ChromeDriver();
-    driver.manage().window().maximize();
-    driver.get(URL_BASE);
+  @BeforeClass
+  public static void prepararTestes() {
+    googlePage = new GooglePO(driver);
   }
 
   @Test
-  public void devePesquisarNoGoogle() {
-    iniciar();
+  public void TC001_deveSerPossivelPesquisarNoGoogleOTextoBatataFrita() {
+    googlePage.pesquisar("Batata frita");
 
-    //Preparação, ação e confirmação..
-    WebElement inputPesquisa = driver.findElement(By.name("q"));
-    inputPesquisa.sendKeys("Batata frita" + Keys.ENTER);
-
-    String resultado = driver.findElement(By.id("result-stats")).getText();
-
+    String resultado = googlePage.obterResultadoDaPesquisa();
     assertTrue(resultado, resultado.contains("Aproximadamente"));
+  }
 
-    driver.quit();
+  @Test
+  public void TC002_deveSerPossivelPesquisarNoGoogleOTextoNutella() {
+    googlePage.pesquisar("Nutella");
+
+    String resultado = googlePage.obterResultadoDaPesquisa();
+
+    assertTrue(resultado, resultado.contains("resultados"));
   }
 }
