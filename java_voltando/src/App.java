c91bd4cc10dc7ae875cc4f651a16a6ddda7d81b4
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2024-04-11 07:50:27 -0300

    commitando2

--- java_voltando/src/App.java
+++ java_voltando/src/App.java
@@ -1,5 +1,17 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+  public static void main(String[] args) throws Exception {
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    double[] vect = new double[n];
+
+    for (int i = 0; i < vect.length; i++) {
+      System.out.println("Marcos");
     }
+  }
 }
