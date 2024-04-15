11718a0cfba4981f764334493573f04d2e819454
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2024-04-15 07:13:02 -0300

    forEach

---
+++ java_treino_abril_2024/treinando_3/treinando4/src/App.java
@@ -0,0 +1,27 @@
+public class App {
+
+  public static void main(String[] args) throws Exception {
+    String[] vect = new String[] { "Maria", "João", "Pedro" };
+
+    double[] vectdouble = new double[] { 1.1, 2.3, 5.6, 5.6, 7.8, 10.0 };
+
+    for (int i = 0; i < vect.length; i++) {
+      System.out.println(vect[i]);
+    }
+
+    System.out.println("-----------------");
+
+    for (String x : vect) {
+      System.out.println(x);
+    }
+
+    System.out.println("-------------------");
+
+    double soma = 0.0;
+    for (Double x : vectdouble) {
+      soma += x;
+    }
+
+    System.out.println(soma);
+  }
+}
